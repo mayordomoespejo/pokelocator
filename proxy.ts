3729890bb2e35350ea8a554a2638323688a89de1
Miguel <miguel@gyoza.es>
2026-02-23 22:27:04 +0100

    chore: replace middleware with proxy

--- proxy.ts
+++ proxy.ts
@@ -42,7 +42,7 @@ function detectLocale(acceptLanguage: string | null): (typeof locales)[number] {
   return defaultLocale;
 }
 
-export default function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const localeInPath = getPathLocale(pathname);
 
